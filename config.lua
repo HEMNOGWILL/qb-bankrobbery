74af47ade700f762dc634d448df9b1e4485c24c7
HemnBahez <hemn_mukriyani@hotmail.com>
2022-05-03 23:23:52 +0200

    changed minimum police for fleecaheist

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.ItemTiers = 1
 
 Config.MinimumPaletoPolice = 4
 Config.MinimumPacificPolice = 5
-Config.MinimumFleecaPolice = 3
+Config.MinimumFleecaPolice = 2 --3
 Config.MinimumThermitePolice = 2
 Config.OutlawCooldown = 5 -- The amount of minutes it takes for the cops to be able to be called again after they were called
 
